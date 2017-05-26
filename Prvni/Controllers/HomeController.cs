9e98f2ff1c8bc25c512fc669b8b80b63e11ac276
PetrStadnik <peta.stadnik@email.cz>
2017-05-26 16:47:56 +0200

    Přidejte soubory projektu.

---
+++ Prvni/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Prvni.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prvni.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpPost]
+        public ActionResult Index(Kalkulacka kalkulacka)
+        {
+            if (ModelState.IsValid)
+            {
+                kalkulacka.Vypocitej();
+            }
+
+            return View(kalkulacka);
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            //Generator generator = new Generator();
+            //ViewBag.Cislo = generator.VratCislo();
+            Kalkulacka kalkulacka = new Kalkulacka();
+            return View(kalkulacka);
+            
+        }
+
+     
+
+
+
+      
+    }
+}
---
+++ Prvni/Models/Kalkulacka.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prvni.Models
+{
+    public class Kalkulacka
+    {
+        [Display(Name = "1. číslo")]
+        [Required(ErrorMessage = "Zadejte prosím číslo")]
+        public double Cislo1 { get; set; }
+        [Display(Name = "2. číslo")]
+        [Required(ErrorMessage = "Zadejte prosím číslo")]
+        public double Cislo2 { get; set; }
+        public double Vysledek { get; set; }
+        public string Operace { get; set; }
+        public List<SelectListItem> MozneOperace { get; set; }
+
+        public Kalkulacka()
+        {
+            MozneOperace = new List<SelectListItem>();
+            MozneOperace.Add(new SelectListItem { Text = "Sečti", Value = "+", Selected = true });
+            MozneOperace.Add(new SelectListItem { Text = "Odečti", Value = "-" });
+            MozneOperace.Add(new SelectListItem { Text = "Vynásob", Value = "*" });
+            MozneOperace.Add(new SelectListItem { Text = "Vyděl", Value = "/" });
+        }
+        public void Vypocitej()
+        {
+            switch (Operace)
+            {
+                case "+":
+                    Vysledek = Cislo1 + Cislo2;
+                    break;
+                case "-":
+                    Vysledek = Cislo1 - Cislo2;
+                    break;
+                case "*":
+                    Vysledek = Cislo1 * Cislo2;
+                    break;
+                case "/":
+                    Vysledek = Cislo1 / Cislo2;
+                    break;
+            }
+        }
+    }
+}
